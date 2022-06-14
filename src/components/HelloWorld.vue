c8e754ec1e2b75875c9e504b8889b339893eb759
kaiburgdorf <k@iburgdorf.de>
2022-06-14 12:00:11 +0200

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,57 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    
+    <div class="row" v-for="row in 20" :key="row">
+      <div class="boardSquare" v-for="square in 10" :key="square">
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+  private board: boolean[];
+
+  constructor() {
+    super();
+        this.board = new Array<boolean>();
+        this.board[0] = true;
+        this.board[1] = true;
+        this.board[2] = true;
+        this.board[3] = true;
+        this.board[4] = true;
   }
+
+  beforeCreate() {
+    
+  }
+
+
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+.boardSquare {
+  width: 48px;
+  height: 48px;
+  border: 1px solid;
+  background-color: #fff;
+  flex-flow: column;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.boardSquare:hover {
+  background-color: grey;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.row {
+  display: flex;
+  background-color: gray;
+  width: 500px;
 }
 </style>
---
+++ src/components/tetris/Tetris.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+/*import { defineComponent } from '@vue/composition-api'
+import Vue from 'vue'
+
+export default Tetris extends Vue ({
+    setup() {
+        
+    },
+})
+</script>
+*/
